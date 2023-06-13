4d5fa9acc26d496d36d79f410e11649ee6a55a32
Carter Medlin <carterm@users.noreply.github.com>
2023-06-12 17:07:38 -0700

    fixing samples folder

--- sample_site/pages/samples/samples.11tydata.js
+++ sample_site/pages/samples/samples.11tydata.js
@@ -2,6 +2,11 @@ module.exports = {
   eleventyComputed: {
     layout: "sample.njk",
     tags: "sample",
+    permalink: data =>
+      `/samples/${
+        /** @type {import("@11ty/eleventy/src/Template")} */ (data.page)
+          .fileSlug
+      }.html`,
     title: data =>
       `${
         /** @type {import("@11ty/eleventy/src/Template")} */ (data.page)
