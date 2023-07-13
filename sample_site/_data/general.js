7ab565fe5626042dcff1eb87bac3e49653e66c6d
Carter Medlin <carterm@users.noreply.github.com>
2023-07-13 14:27:21 -0700

    CDN location update

--- sample_site/_data/general.js
+++ sample_site/_data/general.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CdnPath: "https://california.azureedge.net/cdt/statetemplate/6.2/",
+  CdnPath: "https://california.azureedge.net/cdt/statetemplate/6.2.0/",
   VersionGtmPropertyId: "GTM-NJ6Q4MV",
   StateTemplateVersion: "6.2"
 };
