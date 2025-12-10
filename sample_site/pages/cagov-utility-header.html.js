d015b96e5e7032efc2972a4f8a50f52da62101ff
Carter Medlin <carterm@users.noreply.github.com>
2025-12-10 14:42:27 -0800

    polyfill pre-test

--- sample_site/pages/cagov-utility-header.html.js
+++ sample_site/pages/cagov-utility-header.html.js
@@ -1,22 +1,29 @@
 //@ts-check
 document.addEventListener("DOMContentLoaded", () => {
-  // Polyfill for details element to allow only one open at a time per name attribute
-  /** @type {NodeListOf<HTMLDetailsElement>} */
-  const accordions = document.querySelectorAll("details[name]");
-  accordions.forEach(details => {
-    details.addEventListener("toggle", () => {
-      if (details.open) {
-        accordions.forEach(other => {
-          if (
-            other.open &&
-            other !== details &&
-            other.getAttribute("name") === details.getAttribute("name")
-          ) {
-            other.open = false;
-            console.log("POLYFILL: details closed");
-          }
-        });
-      }
+  // Test to see if browser supports details.name
+  const test = document.createElement("details");
+  test.setAttribute("name", "x");
+  const supportsName = test.name === "x";
+
+  if (!supportsName) {
+    // Polyfill for details element to allow only one open at a time per name attribute
+    /** @type {NodeListOf<HTMLDetailsElement>} */
+    const accordions = document.querySelectorAll("details[name]");
+    accordions.forEach(details => {
+      details.addEventListener("toggle", () => {
+        if (details.open) {
+          accordions.forEach(other => {
+            if (
+              other.open &&
+              other !== details &&
+              other.getAttribute("name") === details.getAttribute("name")
+            ) {
+              other.open = false;
+              console.log("POLYFILL: details.name closed");
+            }
+          });
+        }
+      });
     });
-  });
+  }
 });
