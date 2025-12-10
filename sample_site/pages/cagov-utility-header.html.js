7bbc0d6baf163e3e73e3f3d35f273c0cb9d48802
Carter Medlin <carterm@users.noreply.github.com>
2025-12-10 14:24:59 -0800

    nocode

--- sample_site/pages/cagov-utility-header.html.js
+++ sample_site/pages/cagov-utility-header.html.js
@@ -1,30 +1,5 @@
 //@ts-check
 document.addEventListener("DOMContentLoaded", () => {
-  /** @type {HTMLDetailsElement} */
-  const menu = document.querySelector("details.cagov-utility-menu-button");
-
-  /** @type {HTMLDivElement} */
-  const dropdown = document.querySelector("div.collapsible-content");
-
-  /** @type {HTMLDetailsElement} */
-  const menu2 = document.querySelector("details.cagov-utility-search-button");
-
-  /** @type {HTMLDivElement} */
-  const dropdown2 = document.querySelector("div.cagov-search");
-
-  const checkMenus = () => {
-    dropdown.style.display = menu.open ? "block" : "none";
-    dropdown2.style.display = menu2.open ? "block" : "none";
-  };
-
-  [menu, menu2].forEach(x =>
-    x.addEventListener("toggle", () => {
-      checkMenus();
-    })
-  );
-
-  checkMenus();
-
   // Polyfill for details element to allow only one open at a time per name attribute
   /** @type {NodeListOf<HTMLDetailsElement>} */
   const accordions = document.querySelectorAll("details[name]");
