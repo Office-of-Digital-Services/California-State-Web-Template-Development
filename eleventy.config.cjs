965e15da25d4ba29ad3024883258ea8ebadc5ce2
Carter Medlin <carterm@users.noreply.github.com>
2026-02-20 10:59:57 -0800

    reduce

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -6,8 +6,8 @@ const postcssNested = require("postcss-nested");
 const PurgeCSS = require("@fullhuman/postcss-purgecss");
 
 /**
- * @typedef {import("@11ty/eleventy/src/defaultConfig.js").defaultConfig} EleventyDefaultConfig
- * @typedef {import("@11ty/eleventy/src/UserConfig.js").default} EleventyConfig
+ * @typedef {import("@11ty/eleventy/src/defaultConfig").defaultConfig} EleventyDefaultConfig
+ * @typedef {import("@11ty/eleventy/src/UserConfig").default} EleventyConfig
  */
 
 module.exports = function (/** @type {EleventyConfig} **/ eleventyConfig) {
